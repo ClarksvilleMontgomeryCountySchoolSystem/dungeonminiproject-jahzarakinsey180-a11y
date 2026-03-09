6656da8955555476ea57a33f9c7519d137e760a2
jahzarakinsey180-a11y <jahzara.kinsey180@students.cmcss.net>
2026-03-09 14:17:27 -0500

    j

--- task4.py
+++ task4.py
@@ -1,5 +1,24 @@
+good = r"""
+      _\       
+       /`b  
+  /####J   
+   |\ ||   Asik
+"""
+bad = r"""
+            _.===._      ,"^^^^",
+           /_\^^^/_\    /  ^ ,^  \     ,
+           (0\ ^ /0)\  / ^  /  ^  \    /\
+            \     /  ^^   ^ \ ^ \  ",." /
+             )   (  ^  ^   ^ \   \    ,'
+             (o_o)^    \ ^   ,)  /`^^`
+              ^V^\ ^ \  \_,-"((((
+      jgs     /  /`""/  /
+             (((`   '(((
+"""
 drawbridge_raised = False
 if not drawbridge_raised:
     outcome = "Thunder: We need to cross the bridge. "
+    print(good)
 else:
     outcome = "Doom: We're going to die in here."
+    print(bad)
